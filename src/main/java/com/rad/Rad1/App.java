9395b50b495334d0a3591983ecbeee6c377f9c60
sireeshaashok <sireeshamak@yahoo.com>
2015-09-23 11:45:32 +0530

    Initial version

---
+++ src/main/java/com/rad/Rad1/App.java
@@ -0,0 +1,13 @@
+package com.rad.Rad1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
