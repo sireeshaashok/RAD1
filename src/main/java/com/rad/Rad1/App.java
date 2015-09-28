e095b286940ac07602cafc8297fba32dd8cdf245
sireeshaashok <sireeshamak@yahoo.com>
2015-09-28 12:09:55 +0530

    Jenkins Trigger

--- src/main/java/com/rad/Rad1/App.java
+++ src/main/java/com/rad/Rad1/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!234324332432");
+        System.out.println( "Hello World!Jenkins trigger");
     }
 }
